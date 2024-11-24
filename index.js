239c41ee6c16fb901ff4836d88547486e78fa429
shahroz ahmad <shozimughal9876@gmail.com>
2024-11-24 23:48:39 +0500

    git inital commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs')
+
+
+app.set("view engine" , "ejs")
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(express.static(path.join(__dirname , "public")))
+
+
+app.get("/", function (req, res) {
+    
+    fs.readdir("./files", function (err, files) {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error reading files");
+        } else {
+            const formattedFiles = files.map(file => {
+                const stats = fs.statSync(`./files/${file}`);
+                const details = fs.readFileSync(`./files/${file}`, 'utf-8') || "";
+
+                return {
+                    name: file.replace(".txt", "").replace(/([A-Z])/g, " $1").trim(),
+                    fileNameCamelCase: file, 
+                    details: details ,
+                    mtime: stats.mtime 
+                };
+            })
+            .sort((a, b) => b.mtime - a.mtime); 
+            res.render("index", { files: formattedFiles, error: req.query.error });
+        }
+    });
+});
+
+app.post("/create", function (req, res) {
+    const { title, details } = req.body;
+
+    if (!title && !details) {
+        return res.redirect("/?error=Title and details are required.");
+    }
+    // Convert the title to CamelCase for saving
+    const camelCaseTitle = req.body.title
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+        .join("");
+
+    fs.writeFile(`./files/${camelCaseTitle}.txt`, req.body.details, function (err) {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error creating file");
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+app.get("/file/:filename", function(req, res){
+    const filePath = `./files/${req.params.filename}`;
+    fs.readFile(filePath, 'utf-8', function(err, filedata){
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error reading file");
+        }
+        res.render("show", { 
+            filename: req.params.filename.replace(".txt", "").replace(/([A-Z])/g, " $1").trim(), 
+            filedata: filedata
+        });
+    });
+});
+
+app.post("/delete/:filename" , function(req, res){
+    const filePath = `./files/${req.params.filename}`
+    fs.unlink(filePath , function(err){
+        if(err){
+            console.error(err)
+            res.status(500).send("Error deleting file")
+        }else{
+            res.redirect("/?message=Note+deleted+successfully");        }
+    })
+})
+app.listen(3000)
